05650c47682bddbcfc06811223d6ce6b2bda8f56
Ithai Levi <ithai.levi@gmail.com>
2013-02-14 21:10:55 +0000

    initial petri spec

---
+++ test/petri.c
@@ -0,0 +1,41 @@
+#include "c89spec/c89spec.h"
+#include "../src/petri.h"
+
+describe (petri_module) {
+   it (should create a petri file) {
+
+   }
+
+   it (should open a precreated petri file) {
+
+   }
+
+   it (should not open a non-petri file) {
+
+   }
+
+   it (should close an opened petri file) {
+
+   }
+
+   it (should err trying to close invalid file) {
+
+   }
+
+   it (should index a value in a petri file) {
+
+   }
+
+   it (should replace an indexed value in a petri file) {
+
+   }
+
+   it (should deal with hash collisions gracefully) {
+
+   }
+
+   it (should remove an index from a petri file) {
+
+   }
+}
+
