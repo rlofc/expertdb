39c1c8c9fa1578516f6ef2fd7cd8a6f49a84795d
Ithai Levi <ithai.levi@gmail.com>
2013-02-16 21:28:15 +0200

    initial dfile module spec

---
+++ test/dfile.c
@@ -0,0 +1,30 @@
+#include "c89spec/c89spec.h"
+#include "../src/dfile.h"
+#include <stdio.h>
+describe (dfile_module) {
+
+   it (should create a storage file) { 
+       
+   }
+
+   it (should open an existing storage file) { 
+       
+   }
+
+   it (should fail to open an invalid file) { 
+       
+   }
+
+   it (should close an opened file) { 
+       
+   }
+
+   it (should store records) { 
+       
+   }
+
+   it (should restore records) { 
+       
+   }
+
+}
--- test/test.c
+++ test/test.c
@@ -1,9 +1,11 @@
 #include "c89spec/c89spec.h"
 
 void petri_module();
+void dfile_module();
 
 int main(int argc, char const *argv[])
 {
    test(petri_module);
+   test(dfile_module);
    return summary();
 }
