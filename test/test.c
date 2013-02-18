b47b61d87c913450adaad6f416e301ce60c662d7
Ithai Levi <ithai.levi@gmail.com>
2013-02-18 22:18:54 +0000

    initiating trax - the transactional extension

--- test/test.c
+++ test/test.c
@@ -3,11 +3,13 @@
 void petri_module();
 void dfile_module();
 void gstor_module();
+void trax_module();
 
 int main(int argc, char const *argv[])
 {
    test(petri_module);
    test(dfile_module);
    test(gstor_module);
+   test(trax_module);
    return summary();
 }
---
+++ test/trax.c
@@ -0,0 +1,24 @@
+#include "c89spec/c89spec.h"
+#include <stdio.h>
+
+describe (trax_module) {
+
+   it (should initialize a graph manipulation transaction) {
+      /* trage_begin(gstor_ptr); */
+   }
+
+   it (should conclude a graph manipulation transaction) {
+      /* trage_eng(gstor_ptr); */
+   }
+
+   it (should keep manipulation context atomic) {
+      /* trage_t * t = trage_begin(gstor_ptr); */
+
+      /* trage_add_node(t); */
+      /* trage_del_node(t); */
+      /* trage_link(t); */
+
+      /* trage_end(t); */
+   }
+
+}
